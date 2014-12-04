eb021b8f81fc5c3dcd85253aca5ff272d2be5830
sherryshare <siyuan.sherryshare.wang@gmail.com>
2014-12-04 16:50:25 +0800

    first commit

---
+++ tp8_TBB.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "tbb/flow_graph.h"
+
+using namespace tbb::flow;
+
+struct square {
+  int operator()(int v) { return v*v; }
+};
+
+struct cube {
+  int operator()(int v) { return v*v*v; }
+};
+
+class sum {
+  int &my_sum;
+public:
+  sum( int &s ) : my_sum(s) {}
+  int operator()( tuple< int, int > v ) {
+    my_sum += get<0>(v) + get<1>(v);
+    return my_sum;
+  }
+};
+
+int main() {
+  int result = 0;
+
+  graph g;
+  broadcast_node<int> input(g);
+  function_node<int,bool> squarer( g, unlimited, square() );
+  function_node<int,int> cuber( g, unlimited, cube() );
+  join_node< tuple<int,int>, queueing > join( g );
+  function_node<tuple<int,int>,int>
+      summer( g, serial, sum(result) );
+
+  make_edge( input, squarer );
+  make_edge( input, cuber );
+  make_edge( squarer, get<0>( join.input_ports() ) );
+  make_edge( cuber, get<1>( join.input_ports() ) );
+  make_edge( join, summer );
+
+  for (int i = 1; i <= 10; ++i)
+      input.try_put(i);
+  g.wait_for_all();
+
+  printf("Final result is %d\n", result);
+  return 0;
+}
